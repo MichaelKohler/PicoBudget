9c46b76b37849cffdb85e243b7829ae42ff4fb66
Michael Kohler <michaelkohler@live.com>
2012-06-16 19:05:56 +0200

    Handlebars added + commented route

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ server.configure(function () {
   server.use('/js', express.static(__dirname + '/js'));
   server.use(express.static(__dirname + '/html'));
 });
+server.set('views', __dirname + '/html');
+server.register('.html', require('handlebars'));
+server.set('view options', { layout: false });
 
 server.listen(1337);
+console.log('Express server started on port %s', server.address().port);
 
+/*server.get('/about', function(req, res) {
+  res.render('blubb.html');
+});*/
