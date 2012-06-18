61455337c959fa1a64d21efdd9ab17851005b9e4
Michael Kohler <michaelkohler@live.com>
2012-06-18 03:58:05 +0200

    use Jade as template engine -> all views rewritten

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ server.configure(function () {
   server.use('/bootstrap', express.static(__dirname + '/bootstrap'));
   server.use('/css', express.static(__dirname + '/css'));
   server.use('/js', express.static(__dirname + '/js'));
-  server.use(express.static(__dirname + '/html'));
 });
-server.set('views', __dirname + '/html');
-server.register('.html', require('handlebars'));
-server.set('view options', { layout: false });
+server.set('view engine', 'jade');
+server.set('views', __dirname + '/views');
+server.set('view options', { layout: false }) 
 server.use(express.bodyParser());
 server.use(express.cookieParser({ secret: "keyboard cat" }));
 var memStore = require('connect').session.MemoryStore;
@@ -26,14 +25,14 @@ function requiresLogin(req, res, next) {
     next();
   }
   else {
-    res.redirect('/login'); // TODO: process redirect
+    res.redirect('/login');
   }
 }
 
 var pseudoUsers = require('./users');
 
 server.get('/login', function(req, res) {
-  res.render('login.html');
+  res.render('login');
 });
 server.post('/authenticated', function(req, res) {
   pseudoUsers.authenticate(req.body['emailInput'], req.body['passwordInput'], function(user) {
@@ -49,49 +48,52 @@ server.post('/authenticated', function(req, res) {
 
 /* ROUTERS ****************/
 /**************************/
+server.get('/', function(req, res) {
+  res.render('index', { locals: {user: req.session.user}});
+});
 server.get('/home', function(req, res) {
-  res.render('index.html');
+  res.render('index');
 });
 server.get('/faq', function(req, res) {
-  res.render('faq.html');
+  res.render('faq');
 });
 server.get('/introduction', function(req, res) {
-  res.render('introduction.html');
+  res.render('introduction');
 });
 server.get('/premium', function(req, res) {
-  res.render('premium.html');
+  res.render('premium');
 });
 server.get('/about', function(req, res) {
-  res.render('about.html');
+  res.render('about');
 });
 server.get('/dashboard', requiresLogin, function(req, res) {
-  res.render('dashboard.html');
+  res.render('dashboard');
 });
 server.get('/accounts', requiresLogin, function(req, res) {
-  res.render('accounts.html');
+  res.render('accounts');
 });
 server.get('/transactions', requiresLogin, function(req, res) {
-  res.render('transactions.html');
+  res.render('transactions');
 });
 server.get('/budget', requiresLogin, function(req, res) {
-  res.render('budget.html');
+  res.render('budget');
 });
 server.get('/reports', requiresLogin, function(req, res) {
-  res.render('reports.html');
+  res.render('reports');
 });
 server.get('/settings', requiresLogin, function(req, res) {
-  res.render('settings.html');
+  res.render('settings');
 });
 server.get('/logout', function(req, res) {
   delete req.session.user;
   res.redirect('/login?loggedOut=true');
 });
 server.get('/admin_dashboard', requiresLogin, function(req, res) {
-  res.render('admin_dashboard.html');
+  res.render('admin_dashboard');
 });
 server.get('/admin_users', requiresLogin, function(req, res) {
-  res.render('admin_users.html');
+  res.render('admin_users');
 });
 server.get('/admin_pricing', requiresLogin, function(req, res) {
-  res.render('admin_pricing.html');
+  res.render('admin_pricing');
 });
