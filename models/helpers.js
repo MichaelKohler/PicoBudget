5eaf21220155fa8f6aa288fbdad0bd6a129b899e
Michael Kohler <mkohler@picobudget.com>
2013-04-19 14:24:28 +0200

    use tabs on account page

--- models/helpers.js
+++ models/helpers.js
@@ -14,4 +14,4 @@ module.exports.formatAmount = function (amount) {
   var fixedAmount = parseFloat(amount).toFixed(2);
   var formattedAmount = fixedAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
   return formattedAmount;
-}
+};
