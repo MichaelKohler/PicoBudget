8cc4c786869b45160ecc86e8a463dc8c6d915a91
Michael Kohler <mkohler@picobudget.com>
2013-09-04 21:41:15 +0200

    fix date issue..

--- routes/budget.js
+++ routes/budget.js
@@ -5,6 +5,11 @@ var globals = require('../globals');
 exports.budget = function (req, res) {
   var locals = {user: req.session.user || ''};
   locals.pagetitle = 'Budget - ' + globals.titleAddition;
-  locals.currentMonthYear = Date.toLocaleDateString(new Date());
+  var dateNow = new Date();
+  // TODO: better fix for the names..
+  var monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December' ];
+  locals.currentMonth = monthNames[dateNow.getMonth() - 1];
+  locals.currentYear = dateNow.getFullYear();
   res.render('budget', locals);
 };
