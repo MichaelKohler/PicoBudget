6cf4837432f50fa2e38728777e9c83cc7572364b
Michael Kohler <mkohler@picobudget.com>
2014-07-07 21:05:03 +0200

    use gulp to run jslint

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var jslint = require('gulp-jslint');
+
+gulp.task('jslint', function () {
+  return gulp.src(['./models/*.js', './routes/*.js', './public/js/libs/PBHelperTools.js',
+                   './globals.js', './app.js'])
+      .pipe(jslint({
+        node: true,
+        nomen: true,
+        white: true,
+        evil: true,
+        errorsOnly: false
+      }));
+});
+
+gulp.task('default', ['jslint']);
